ba3ef6bb7ba10fe8b32f5666fdf7edb5f9fe51e8
web3765 <102269227+web3765@users.noreply.github.com>
2023-02-19 15:10:01 +0900

    [Silver III] Title: 주유소, Time: 420 ms, Memory: 36392 KB -BaekjoonHub

---
+++ 백준/Silver/13305. 주유소/주유소.java
@@ -0,0 +1,40 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws IOException {
+
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st;
+		int N = Integer.parseInt(br.readLine());
+		
+		long[] dis = new long[N-1];
+		long[] cost = new long[N];
+
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N-1; i++) 
+			dis[i] = Long.parseLong(st.nextToken());
+		
+		st = new StringTokenizer(br.readLine());
+		for(int i=0; i<N; i++) {
+			cost[i] = Long.parseLong(st.nextToken());
+		}
+		
+		long sum = 0;
+		long minCost = cost[0];
+		
+		for(int i=0; i<dis.length; i++) {
+			if(cost[i] < minCost) {
+				minCost = cost[i];
+			}
+			sum += (minCost * dis[i]);
+		}
+		
+		System.out.println(sum);
+	}
+
+}
